d247f6d308af467ce061e6d30f0fa0af101f43ce
Ness <johnsondavid1107@gmail.com>
2020-10-01 17:41:03 -0400

    final project completed.  Tested all possible outcomes and confirmed it is working properly

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # meastics
+
+
+Ask the user a series of questions
+a) length of password (numeric input)
+b) validate length is between 8 and 128.
+c) type of characters to include - special characters, uppercase, lowercase, numeric(series of yes/no confirms)
+d) Validate the user selected at least one character type.
+
+Any invalid response will terminate password generation and the user must start over.
+
+
+
+Once the user has entered valid responses, generate a password that meets their criteria and display it in the box.
+
+					PseudoCode
+
+
+Ask user a series of questions to determine the type of password. 
+1. How long will the password be (prompt)
+2. Uppercase letters (confirm)
+3. Lowercase letters (confirm)
+4. Special Characters (confirm)
+5. Numbers (confirm)
+
+Check that the user correctly answered the above questions, if not, re ask the questions. 
+
+Create a loop function 
+
+
+Create all variables that I will be using for the program
+Variables will be series of confirm questions and a prompt to be stored later
+
+Create multiple arrays to store all possible outcomes.  Separate at first so that later if a choice is selected, it can be sent to the final password array.
+
+Create a final password array empty, so that the previous arrays can push depending on user input. 
+
+Create multiple functions to push character to final password array depending on input. If the user does not want a category in their password, the program should skip it and remove all possibilities from final password. 
+
+
+Create series of if and for statements to achieve program using stored variable data from user to push character to final password, or remove the category entirely from possible outcome. 
+
+Because only 1 character can be pushed to final password array per choice section, program needs to fill in the spots remaining randomly based on categories selected by user. For loop built to fill in resining spaces but must consider the length of password originally chosen by user at start of program. 
+
+Console.log totalOptions and final password to confirm it is working properly.  Once confirmed it is working effectively, must going all individual character strings into one string.  
+
+Function created to push final password into the o'clock event listener.  
---
+++ script.js
@@ -0,0 +1,156 @@
+// Assignment Code
+// All Variable----------------------------------
+var generateBtn = document.querySelector("#generate");
+
+for (var i = 0; i < 1; i++) {
+
+  var upperLetters = confirm("Would you like UPPERCASE LETTERS?");
+
+  var lowerLetters = confirm("Would you like lowercaseletters?");
+
+  var numbers1 = confirm("Would you like numbers 867-5309?");
+
+  var specialText = confirm("Would you like Special Characters !@#$%?");
+
+
+  var length = prompt("How long would you like your passowrd to be?  Select between 8 and 12 characters long.");
+
+  if (upperLetters == false && lowerLetters == false && numbers1 == false && specialText == false || length != 8 ||length != 9 ||length != 11 ||length != 12) {
+    alert("Invalid options.  Cannot generate password.  Please try again.");
+    i--;
+  }
+
+
+}
+
+var capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+
+var lowerCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase().split('');
+
+var numbers = "0123456789".split('');
+
+var specialCharacters = "!@#$%^&*".split('');
+
+var finalPassword = [];
+var totalOptions = capitalLetters.concat(lowerCaseLetters, numbers, specialCharacters);
+
+// End of All Variables--------------------------------------
+
+//  All functions ---------------------------------------
+
+function getPasswordCap() {
+  finalPassword.push(capitalLetters[Math.floor(Math.random() * capitalLetters.length)]);
+}
+function getPasswordLow() {
+  finalPassword.push(lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)]);
+}
+function getPasswordNum() {
+  finalPassword.push(numbers[Math.floor(Math.random() * numbers.length)]);
+}
+function getPasswordSpe() {
+  finalPassword.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
+}
+function getPasswordAll() {
+  finalPassword.push(totalOptions[Math.floor(Math.random() * totalOptions.length)]);
+}
+
+
+
+// End of all Functions ------------------------------------------
+//  All if and for Statements--------------------------------
+
+for (var i = 0; i < 1; i++) {
+
+  if (upperLetters == true) {
+    getPasswordCap();
+
+  } else {
+    totalOptions.splice(0, 26);
+
+  }
+}
+console.log(totalOptions);
+console.log(finalPassword);
+for (var i = 0; i < 1; i++) {
+
+  if (lowerLetters == true) {
+    getPasswordLow();
+
+  } else if (upperLetters == false) {
+
+    totalOptions.splice(0, 26);
+
+  } else {
+    totalOptions.splice(26, 26);
+
+  }
+}
+
+for (var i = 0; i < 1; i++) {
+
+  if (numbers1 == true) {
+    getPasswordNum();
+
+  } else if (upperLetters == false && lowerLetters == false) {
+    totalOptions.splice(0, 10);
+
+
+  } else if (upperLetters == true && lowerLetters == false || upperLetters == false && lowerLetters == true) {
+    totalOptions.splice(26, 10);
+  } else if (upperLetters == true && lowerLetters == true && numbers1 == false) {
+    totalOptions.splice(52, 10);
+
+  }
+}
+
+for (var i = 0; i < 1; i++) {
+
+  if (specialText == true) {
+    getPasswordSpe();
+
+  } else if (upperLetters == true && lowerLetters == false && numbers1 == false && specialText == false) {
+    totalOptions.splice(26, 8);
+
+  } else if (upperLetters == true && lowerLetters == true && numbers1 == true && specialText == false) {
+
+    totalOptions.splice(62, 8)
+  }
+  else if (upperLetters == true && lowerLetters == true && numbers1 == false && specialText == false) {
+
+    totalOptions.splice(52, 10);
+  }
+  else if (upperLetters == false && lowerLetters == false && numbers1 == true && specialText == false) {
+
+    totalOptions.splice(10, 8);
+
+  }
+  
+}
+
+console.log(totalOptions);
+console.log(finalPassword);
+for (var i = finalPassword.length; i < length; i++) {
+  finalPassword.push(totalOptions[Math.floor(Math.random() * totalOptions.length)]);
+
+}
+
+// End of all if and for statments ----------------------------
+
+// Joing multi string randomized password into one strin --------
+
+var newPassword = finalPassword.join("");
+
+
+//  CONFIRMING FOR DEVELOPERS PASSWORD GENERATES CORRECTLY ------
+
+// Write password to the #password input
+function writePassword() {
+  var password = newPassword
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
